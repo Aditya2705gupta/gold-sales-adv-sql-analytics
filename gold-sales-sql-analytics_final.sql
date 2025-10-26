8508fe43224c956e71b3b7210ac3729c53c819b6
Aditya2705gupta <157627109+Aditya2705gupta@users.noreply.github.com>
2025-10-26 23:23:15 +0530

    Update gold-sales-sql-analytics_final.sql

--- gold-sales-sql-analytics_final.sql
+++ gold-sales-sql-analytics_final.sql
@@ -24,15 +24,6 @@ from gold_facts_sales
 group by year(order_date)
 order by year(order_date) ; 
 
-select
-year(order_date) as order_year, 
-month(order_date) as order_month, 
-sum(sales_amount) as total_sales,
-count(distinct customer_key) as total_customers,
-sum(quantity) as total_quantity
-from gold_facts_sales 
-group by year(order_date), month(order_date)
-order by year(order_date), month(order_date) ; 
 
 
 select
